ad18ed247080f11f898a73a3c0b1f8483789035f
Saebekassebil <saebekassebil@gmail.com>
2012-09-02 12:36:11 +0200

    Dynamic spacing, and a new 'name' option

--- corda.js
+++ corda.js
@@ -31,11 +31,12 @@
     ctx.restore();
     ctx.closePath();
   }
-  
+
   function CordaSymbol(options) {
     this.options = options;
     this.fret = options.fret || 0;
     this.notes = options.notes;
+    this.name = options.name;
     this.styles = {};
 
     // Copy main style, and apply private styling
@@ -48,32 +49,47 @@
   }
 
   CordaSymbol.prototype = {
+    /**
+     * #render(DOMElement canvas)
+     *  - Renders the chord symbol onto a <canvas> element
+     **/
     render: function(canvas) {
       var   ctx = canvas.getContext('2d')
           , height = canvas.height
           , width = canvas.width
-          , frets = 6
-          , fretUnit = height / frets
+          , options = this.options
+          , frets = options.frets || 5
+          , strings = options.strings || 6
+          , fretUnitY = height / (frets + 1)
+          , fretUnitX = width / strings
+          , fretUnit = Math.min(fretUnitY, fretUnitX)
           , marginLeft = fretUnit / 2
           , marginRight = fretUnit / 2
           , marginTop = fretUnit
           , symbolWidth = width - marginLeft - marginRight - 1
           , symbolHeight = height - marginTop - 1
-          , strings = 6
           , stringUnit = symbolWidth / (strings - 1)
           , nutWidth = fretUnit / 4
           , fontSize = fretUnit / 3 * 2
+          , notes = this.notes
           , i, length, x, y, yshift, text, tuning;
+
 
       ctx.save();
       ctx.font = fontSize + 'px ' + this.styles.font;
 
-      // Fret number
-      if (this.options.showFret && this.fret !== 0) {
+      if (options.name) {
+        marginTop += fontSize;
+        symbolHeight = height - marginTop - 1;
+      }
+
+      // Render the fret number
+      if (options.showFret && this.fret !== 0) {
         text = this.fret.toString(10) + 'fr';
-        marginRight += ctx.measureText(text).width + stringUnit / 2;
-        symbolWidth = width - marginLeft - marginRight - 1;
+        marginRight += ctx.measureText(text).width;
+        symbolWidth = width - marginRight - marginLeft - 1;
         stringUnit = symbolWidth / (strings - 1);
+
         ctx.fillText(
             text,
             symbolWidth + stringUnit,
@@ -81,18 +97,31 @@
         );
       }
 
-      // If a tuning is supplied, lets show it!
-      if ((tuning = this.options.tuning)) {
+      if (options.name) {
+        ctx.save();
+        text = options.name;
+        ctx.font = 'bold ' + fontSize + 'px ' + this.styles.font;
+        ctx.fillText(
+            text,
+            marginLeft + symbolWidth / 2 - ctx.measureText(text).width / 2,
+            fontSize
+        );
+        ctx.restore();
+      }
+
+      // Render the tuning if supplied
+      if ((tuning = options.tuning)) {
         for (i = 0, length = tuning.length; i < length; i++) {
+          text = tuning[i];
           ctx.fillText(
-              tuning[i],
-              marginLeft + stringUnit * i - ctx.measureText(tuning[i]).width / 2,
+              text,
+              marginLeft + stringUnit * i - ctx.measureText(text).width / 2,
               marginTop - fontSize / 2
           );
         }
       }
 
-      // Strings
+      // Render the strings
       ctx.lineWidth = 1;
       ctx.strokeStyle = this.styles.string;
       for (i = 0; i < strings; i++) {
@@ -104,48 +133,70 @@
         ctx.stroke();
       }
 
-      // Frets
-      yshift = symbolHeight / (frets - 1); 
+      // Render the frets
+      yshift = symbolHeight / frets;
       ctx.strokeStyle = this.styles.fret;
-      for (i = 0; i < frets; i++) {
+      for (i = 0; i < frets + 1; i++) {
         ctx.save();
         y = marginTop + i * yshift;
+        x = marginLeft;
 
         if (this.fret === 0 && i === 0) {
           ctx.strokeStyle = this.styles.nut;
           ctx.lineWidth = nutWidth;
+          x -= 0.5;
         }
 
         ctx.beginPath();
-        ctx.moveTo(marginLeft, y);
-        ctx.lineTo(width - marginRight, y);
+        ctx.moveTo(x, y);
+        ctx.lineTo(width - marginRight - 0.5, y);
         ctx.closePath();
         ctx.stroke();
         ctx.restore();
       }
 
-      // Notes
-      var notes = this.notes;
-      y = marginTop / 2;
+      // Render the notes
       for (i = 0; i < notes.length; i++) {
         ctx.save();
         x = marginLeft + i * stringUnit;
+        y = marginTop;
         if (notes[i] === 'x') {
           ctx.lineWidth = 2;
           ctx.strokeStyle = this.styles.cross;
-          renderCross(ctx, x, y - nutWidth / 2, fretUnit / 3 * 2);
+          renderCross(ctx, x, y - fretUnit / 3 - nutWidth, fretUnit / 3 * 2);
         } else if (notes[i] === 'o' || notes[i] === 0) {
           ctx.lineWidth = 2;
           ctx.strokeStyle = this.styles.open;
-          renderCircle(ctx, x, y - nutWidth / 2, fretUnit / 3, true);
+          renderCircle(ctx, x, y - fretUnit / 3 - nutWidth, fretUnit / 3, true);
         } else if (!!notes[i]) {
           ctx.fillStyle = this.styles.dot;
-          renderCircle(ctx, x, y + fretUnit * notes[i], fretUnit / 3, false);
+          y += -yshift / 2 + yshift * notes[i];
+          renderCircle(ctx, x, y, fretUnit / 3, false);
         }
         ctx.restore();
       }
 
       ctx.restore();
+    },
+
+    /**
+     * #style(Object/String key, [String val])
+     *  - Sets the specific style of the chord symbol.
+     *    *key* can either be a table of styles or a style-key
+     *    *val* is only used if key is a string
+     **/
+    style: function(key, val) {
+      // A table of style values
+      if (typeof key === 'object') {
+        for (var i in key) {
+          if (key.hasOwnProperty(i)) {
+            this.styles[i] = key[i];
+          }
+        }
+      } else if (key in this.styles) {
+        // Just a single style value
+        this.styles[key] = val;
+      }
     }
   };
 
@@ -159,12 +210,12 @@
       if (typeof key === 'object') {
         for (var i in key) {
           if (key.hasOwnProperty(i)) {
-            Corda.styles[i] = key[i];
+            this.styles[i] = key[i];
           }
         }
-      } else if (key in Corda.styles) {
+      } else if (key in this.styles) {
         // Just a single style value
-        Corda.styles[key] = val; 
+        this.styles[key] = val;
       }
     },
 
@@ -178,7 +229,7 @@
       cross: '#000000'
     }
   };
-  
+
   Corda.CordaSymbol = CordaSymbol;
   window.Corda = Corda;
 })();
